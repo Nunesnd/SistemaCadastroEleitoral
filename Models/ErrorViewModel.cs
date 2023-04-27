92e972cb5cc0d6743f602a6e9bdc75f8b6fb15f7
Diego Nunes <nunesdiegond@gmail.com>
2023-04-27 15:26:31 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaCadastroEleitoral.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
